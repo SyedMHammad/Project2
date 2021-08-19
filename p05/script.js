92f95d2e0ea2038451a44c9ade3b0006be61de1a
SyedMHammad <smhammad163@gmail.com>
2021-08-19 18:43:28 +0500

    50% work completed
    -adduser
    -doublewealth
    -millionaires

---
+++ p05/script.js
@@ -0,0 +1,70 @@
+const adduser=document.getElementById("add-user");
+const double=document.getElementById("double");
+const milli=document.getElementById("millionaires");
+const sort=document.getElementById("sort");
+const all=document.getElementById("all");
+const main=document.getElementById("main");
+let array=[];
+
+async function Randomuser()
+{
+    let res = await fetch("https://randomuser.me/api/");
+    let data= await res.json();
+    let user= data.results[0];
+    const newuser={
+        name: `${user.name.title} ${user.name.first} ${user.name.last}`,
+        wealth: Math.floor((Math.random())*1000000)
+    };
+    addnewuser(newuser);
+
+};
+
+function addnewuser(newuser)
+{
+    array.push(newuser);
+    updateDOM();
+};
+
+function updateDOM(userdata=array)
+{
+    main.innerHTML='<h2><strong>USER </strong>WEALTH</h2>';
+    userdata.forEach(user =>
+        {
+            const divElement=document.createElement('div');
+            divElement.classList.add('user');
+            divElement.innerHTML=`<strong>${user.name} </strong>$ ${(user.wealth).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')}`;
+            main.appendChild(divElement);
+
+
+        }
+    )
+ 
+}
+
+adduser.addEventListener('click',Randomuser);
+
+function doublewealth()
+{
+    array = array.map(user=>{
+            return{ ...user,wealth: user.wealth*2};
+        })
+    updateDOM();    
+}
+
+double.addEventListener('click',doublewealth);
+
+function mil()
+{
+    array=array.filter(user => user.wealth>1000000);
+    updateDOM();
+}
+
+milli.addEventListener('click',mil);
+
+function sorting()
+{
+    array=array.sort();
+    updateDOM();
+}
+
+sort.addEventListener('click',sorting);
