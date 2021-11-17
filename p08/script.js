787a3ddfff244308a00525d2416aedf390e97564
SyedMHammad <smhammad163@gmail.com>
2021-11-18 02:16:07 +0500

    class1 completed

---
+++ p08/script.js
@@ -0,0 +1,55 @@
+const search=document.getElementById("searchbox");
+const sbtn=document.getElementById("search");
+const random=document.getElementById("random");
+const resultheading=document.getElementById("resultheading");
+const searchresult=document.getElementById("searchresult");
+const selectedmeal=document.getElementById("selectedmeal");
+const notification=document.getElementById("notification");
+
+function searchmeal(e)
+{
+    e.preventDefault();
+    if(search.value.trim())
+    {
+        const searchres=search.value;
+
+        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${search.value}`)
+            .then(res => res.json())
+            .then(data=>{
+                if(data.meals===null)
+                {
+                    resultheading.innerHTML=`
+                    <h2>No result found for "${searchres}"</h2>`;
+                    searchresult.innerHTML=``;
+                }
+                else
+                {
+                    resultheading.innerHTML=`
+                    <h2>Search Results for "${search.value}"</h2>`;
+                    console.log(data.meals);
+                    searchresult.innerHTML = data.meals.map( meal => `
+                    <div class="meal">
+                        <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+                        <div class="meal-info" data-mealID="${meal.idMeal}">
+                            <h3>${meal.strMeal}</h3>
+                        </div>
+                    </div>
+                `)
+                .join('');
+                    search.value='';
+                }
+            });
+            
+            
+
+    }
+    else
+    {
+
+        notification.classList.add('show');
+        setTimeout(()=>{notification.classList.remove('show')},1000);
+
+    }
+}
+search.addEventListener("submit",searchmeal);
+sbtn.addEventListener("click",searchmeal);
