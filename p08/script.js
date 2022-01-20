8e5dd3ce2ab76350004172a5f4c03babc66a12fe
SyedMHammad <smhammad163@gmail.com>
2022-01-21 01:00:57 +0500

    Completed

--- p08/script.js
+++ p08/script.js
@@ -9,6 +9,7 @@ const notification=document.getElementById("notification");
 function searchmeal(e)
 {
     e.preventDefault();
+    selectedmeal.innerHTML='';
     if(search.value.trim())
     {
         const searchres=search.value;
@@ -51,12 +52,48 @@ function searchmeal(e)
 
     }
 }
+function renderMeal(mealsfetched)
+{
+
+    resultheading.innerHTML='';
+    searchresult.innerHTML='';
+    const ingredients=[];
+    for( let i=1;i<=20;i++)
+    {
+        if(mealsfetched[`strIngredient${i}`])
+        {
+            ingredients.push(`${mealsfetched[`strIngredient${i}`]} - ${mealsfetched[`strMeasure${i}`]}`);
+        }
+        else
+        {
+            break;
+        }
+    }
+    console.log(mealsfetched);
+    selectedmeal.innerHTML=`
+    <div>
+    <h1>${mealsfetched.strMeal}</h1>
+    <img class="newimage" src="${mealsfetched.strMealThumb}" alt="${mealsfetched.strMeal}" />
+    </div>
+    <div class="mealdetails">
+    <h2>Ingredients</h2>
+    <ul>
+    ${ingredients.map(ingredient=>`<li>${ingredient}</li>`).join('')}
+    </ul>
+    <h2>Method</h2>
+    <p>${mealsfetched.strInstructions}</p>
+    <h4>Visit the following link for the video:<a href=${mealsfetched.strYoutube}> ${mealsfetched.strYoutube}</a></h4>
+    </div>
+    
+    `
+}
 function getdetails(mealID)
 {
     fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
     .then(res => res.json())
     .then(data=>{
-        console.log(data);
+        const mealsfetched=data.meals[0];
+        renderMeal(mealsfetched);
     })
 }
 search.addEventListener("submit",searchmeal);
